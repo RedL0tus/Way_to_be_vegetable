2b37cfb462e44f38f6b3cadacec9430f02bdbea6
Kay Miller <i@ikay.work>
2017-06-18 15:30:33 +0800

    [Squashed] Add reStructuredText support & Bye-bye TravisCI
    (。・・)ノ

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # RedL0tus.github.io
 
-[![Build Status](https://travis-ci.org/RedL0tus/Way_to_be_vegetable.svg?branch=master)](https://travis-ci.org/RedL0tus/Way_to_be_vegetable)
 Just a [blog](https://v2bv.win/).
---
+++ script/cibuild.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -e # halt script on error
+
+python --version
+pip --version
+echo -e "[\033[32m OK \033[0m] Found Python."
+
+pip install docutils pygments
+echo -e "[\033[32m OK \033[0m] Python packages installed."
+
+bundle install
+gem install RbST
+echo -e "[\033[32m OK \033[0m] Dependencies installed."
+
+bundle exec jekyll build
+echo -e "[\033[32m OK \033[0m] Site generated."
+
+pygmentize -S default -f html > _site/css/pygments.css
+echo -e "[\033[32m OK \033[0m] Code highlighting CSS generated."
